5add4388d0645ba4014307e0f61dd183225b7b69
desirelovell <desirelovell@outlook.com>
2024-08-08 13:30:59 -0500

    Initial Commit

---
+++ quietcouture/quietcoutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  quietcoutureApp.swift
+//  quietcouture
+//
+//  Created by Desiree Lovell on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct quietcoutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quietcoutureUITests/quietcoutureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  quietcoutureUITestsLaunchTests.swift
+//  quietcoutureUITests
+//
+//  Created by Desiree Lovell on 8/8/24.
+//
+
+import XCTest
+
+final class quietcoutureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
